66057e091417c453ed65e002394cd4941e330e1d
Dina <dina2203@iitu.edu.kz>
2025-03-24 22:57:42 +0500

    SecondScreen

---
+++ app/src/main/java/com/example/navigationapplab5/screens/SecondScreen.kt
@@ -0,0 +1,28 @@
+package com.example.navigationapplab5.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF3F51B5)),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = { navController.navigate("third_screen") }
+        ) {
+            Text(text = "BUTTON")
+        }
+    }
+}
